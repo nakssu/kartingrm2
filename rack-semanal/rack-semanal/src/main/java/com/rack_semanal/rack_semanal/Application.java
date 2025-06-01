7a01fb2165d15730cfbb89cf87761cbe3d8917fb
nakssu <josepha.gaete@usach.cl>
2025-06-01 03:09:13 -0400

    cambios properties y applications

--- rack-semanal/rack-semanal/src/main/java/com/rack_semanal/rack_semanal/Application.java
+++ rack-semanal/rack-semanal/src/main/java/com/rack_semanal/rack_semanal/Application.java
@@ -2,8 +2,10 @@ package com.rack_semanal.rack_semanal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
--- tarifa-reserva/tarifa-reserva/src/main/java/com/tarifa_reserva/tarifa_reserva/Application.java
+++ tarifa-reserva/tarifa-reserva/src/main/java/com/tarifa_reserva/tarifa_reserva/Application.java
@@ -2,8 +2,10 @@ package com.tarifa_reserva.tarifa_reserva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
